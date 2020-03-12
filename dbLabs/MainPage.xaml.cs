66eaedc39a30c17d08a7525f3fca7bd41ad63c9d
Danila <danila.litskevich@gmail.com>
2020-03-12 21:31:15 +0300

    added system.data.extension...      !!!!!🔥🔥🔥🔥🔥🔥

--- dbLabs/MainPage.xaml.cs
+++ dbLabs/MainPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 using Xamarin.Forms;
+using System.Configuration;
 
 namespace dbLabs {
 	// Learn more about making custom code visible in the Xamarin.Forms previewer
@@ -14,13 +15,41 @@ namespace dbLabs {
 	[DesignTimeVisible (false)]
 	public partial class MainPage : ContentPage {
 		private DataSet shopDS = new DataSet("Shop");
-		private string connString = string.Empty;
+		private string connString = "server=127.0.0.1; user=root; password=Password; database=shop_example";
 		private MySqlDataAdapter manufAdapter;
 		private MySqlDataAdapter productAdapter;
 
-		public MainPage ()
-		{
+		private MySqlCommandBuilder manufCommands;
+		private MySqlCommandBuilder productCommands;
+
+		public MainPage (){
 			InitializeComponent ();
+			manufAdapter = new MySqlDataAdapter("select * from manufact", connString);
+			productAdapter = new MySqlDataAdapter("select * from product", connString);
+
+			manufCommands = new MySqlCommandBuilder(manufAdapter);
+			productCommands = new MySqlCommandBuilder(productAdapter);
+
+			manufAdapter.Fill(shopDS,"manufact");
+			productAdapter.Fill(shopDS, "product");
+
+			DataRelation manufToProduct = new DataRelation("ManufProduct",
+				shopDS.Tables["manufact"].Columns["manuf_id"],
+				shopDS.Tables["product"].Columns["prod_manuf_id"]);
+
+			shopDS.Relations.Add(manufToProduct);
+
+		}
+
+		private void PageLoaded(object sender, EventArgs e) {
+			manufGrid.ItemsSource = shopDS.Tables["manufact"].DefaultView;
+		}
+
+		private void testbutton_Click(object sender, EventArgs e) {
+			var result = shopDS.Tables["manufact"].Select();
+			resultGrid.IsVisible = true;
+			DataTable dt = new DataTable();
+			resultGrid.ItemsSource = result.CopyToDataTable<DataRow>();
 		}
 	}
 }
