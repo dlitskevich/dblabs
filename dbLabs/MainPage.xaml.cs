e549c931ad67959b2b57333ef2f48a5a41acf910
Danila <danila.litskevich@gmail.com>
2020-03-13 22:46:32 +0300

    not working update

--- dbLabs/MainPage.xaml.cs
+++ dbLabs/MainPage.xaml.cs
@@ -18,20 +18,25 @@ namespace dbLabs {
 		private string connString = "server=127.0.0.1; user=root; password=Password; database=shop_example";
 		private MySqlDataAdapter manufAdapter;
 		private MySqlDataAdapter productAdapter;
+		private MySqlDataAdapter customerAdapter;
 
 		private MySqlCommandBuilder manufCommands;
 		private MySqlCommandBuilder productCommands;
+		private MySqlCommandBuilder customerCommands;
 
 		public MainPage (){
 			InitializeComponent ();
 			manufAdapter = new MySqlDataAdapter("select * from manufact", connString);
 			productAdapter = new MySqlDataAdapter("select * from product", connString);
+			customerAdapter = new MySqlDataAdapter("select * from customer", connString);
 
 			manufCommands = new MySqlCommandBuilder(manufAdapter);
 			productCommands = new MySqlCommandBuilder(productAdapter);
+			customerCommands = new MySqlCommandBuilder(customerAdapter);
 
 			manufAdapter.Fill(shopDS,"manufact");
 			productAdapter.Fill(shopDS, "product");
+			customerAdapter.Fill(shopDS, "customer");
 
 			DataRelation manufToProduct = new DataRelation("ManufProduct",
 				shopDS.Tables["manufact"].Columns["manuf_id"],
@@ -43,13 +48,103 @@ namespace dbLabs {
 
 		private void PageLoaded(object sender, EventArgs e) {
 			manufGrid.ItemsSource = shopDS.Tables["manufact"].DefaultView;
+			resultGrid.ItemsSource = shopDS.Tables["product"];
 		}
 
-		private void testbutton_Click(object sender, EventArgs e) {
-			var result = shopDS.Tables["manufact"].Select();
+		//private void testbutton_Click(object sender, EventArgs e) {
+		//	//var result = shopDS.Tables["manufact"].Select("manuf_name = 'dbDealer'");
+
+		//	/*
+		//	var propertyCollection = manufGrid.View.GetPropertyAccessProvider();
+		//	int compID = (int)propertyCollection.GetValue(manufGrid.SelectedItem, "id");
+		//	*/
+		//	/*
+		//	var compID = (int)(manufGrid.SelectedItem as DataRowView).Row.ItemArray[0];
+		//	var result = shopDS.Tables["manufact"].Select($"manuf_id = '{compID}'")[0].GetChildRows("ManufProduct");
+		//	*/
+		//	/* !
+		//	DataView quantityView = new DataView(shopDS.Tables["product"]) {
+		//		RowFilter = "prod_quantity > 410"
+		//	};*/
+		//	var result = from m in shopDS.Tables["product"].AsEnumerable()
+		//				 where ((string)m["prod_type"] == "entertaining" && (int)m["prod_quantity"] > 1000)
+		//				 select m;
+		//	resultGrid.IsVisible = true;
+		//	resultGrid.ItemsSource = result.CopyToDataTable().DefaultView;
+		//}
+		
+		private void Filter(object sender, EventArgs e) {
+			
+			var result = from m in shopDS.Tables["product"].AsEnumerable()
+						where
+						(float.TryParse(minPrice.Text, out float min) ? min : 0) < (float)m["prod_price"]
+						&& (float)m["prod_price"] < (float.TryParse(maxPrice.Text, out float max) ? max : 200)						
+					 select m;
+
+			IEnumerable<DataRow> resultQuery;
+			if(byType.IsChecked && byName.IsChecked) {
+				resultQuery = result.OrderBy(x => x.ItemArray[2]).ThenBy(x => x.ItemArray[1]);
+			}else if(byName.IsChecked) {
+				resultQuery = result.OrderBy(x => x.ItemArray[1]);
+			} else if(byType.IsChecked) {
+				resultQuery = result.OrderBy(x => x.ItemArray[2]);
+			} else {
+				resultQuery = result;
+			};
+ 
+
 			resultGrid.IsVisible = true;
-			DataTable dt = new DataTable();
-			resultGrid.ItemsSource = result.CopyToDataTable<DataRow>();
+			resultGrid.ItemsSource = resultQuery.CopyToDataTable().DefaultView;
+		}
+
+		private void FindProduct(object sender, EventArgs e) {
+
+			var result = from m in shopDS.Tables["product"].AsEnumerable()
+						 where ((string)m["prod_name"]).Contains(productPattern.Text.ToString())
+						 select m;
+
+			IEnumerable<DataRow> resultQuery;
+			if(byType.IsChecked && byName.IsChecked) {
+				resultQuery = result.OrderBy(x => x.ItemArray[2]).ThenBy(x => x.ItemArray[1]);
+			} else if(byName.IsChecked) {
+				resultQuery = result.OrderBy(x => x.ItemArray[1]);
+			} else if(byType.IsChecked) {
+				resultQuery = result.OrderBy(x => x.ItemArray[2]);
+			} else {
+				resultQuery = result;
+			};
+
+
+			resultGrid.IsVisible = true;
+			resultGrid.ItemsSource = resultQuery;
+		}
+
+		private void Refresh(object sender, EventArgs e) {
+
+
+			//manufAdapter.Update(shopDS, "manufact");
+			resultGrid.EndEdit();
+			productAdapter.Update(shopDS,"product");
+
+			shopDS.Tables["product"].AcceptChanges();
+			productAdapter.Fill(shopDS.Tables["product"]);
+			//shopDS.Clear();
+			//manufAdapter.Fill(shopDS, "manufact");
+			//productAdapter.Fill(shopDS, "product");
+
+			resultGrid.IsVisible = true;
+		}
+
+		private void AddProduct(object sender, EventArgs e) {
+
+			var result = from m in shopDS.Tables["product"].AsEnumerable()						
+						 select m;
+
+			
+			
+
+			resultGrid.IsVisible = true;
+			resultGrid.ItemsSource = result.CopyToDataTable().DefaultView;
 		}
 	}
 }
