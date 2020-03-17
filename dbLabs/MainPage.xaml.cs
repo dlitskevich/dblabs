d280b28f0912fe3021617b55e8ae26456ab0ca4c
Danila <danila.litskevich@gmail.com>
2020-03-17 15:51:58 +0300

    1 fifh left

--- dbLabs/MainPage.xaml.cs
+++ dbLabs/MainPage.xaml.cs
@@ -70,145 +70,55 @@ namespace dbLabs {
 			resultGrid.ItemsSource = result;
 		}
 
-		/*
-		private void Filter(object sender, EventArgs e) {
+		private void Russians(object sender, EventArgs e) {
+			var result = from lang in worldDS.Tables["countrylanguage"].AsEnumerable()
+						 group lang by new { id=lang.Field<string>("Language") } into langGrouped
+						 where (langGrouped.Key.id).ToLower() == "russian"
+						 select new {
+							 Language = langGrouped.Key.id,
+							 Quantity = langGrouped.Count()
+						 };
 
-			var result = from m in shopDS.Tables["product"].AsEnumerable()
-						 where
-						 (float.TryParse(minPrice.Text, out float min) ? min : 0) < (float)m["prod_price"]
-						 && (float)m["prod_price"] < (float.TryParse(maxPrice.Text, out float max) ? max : 200)
-						 select m;
-
-			IEnumerable<DataRow> resultQuery;
-			if(byType.IsChecked && byName.IsChecked) {
-				resultQuery = result.OrderBy(x => x.ItemArray[2]).ThenBy(x => x.ItemArray[1]);
-			} else if(byName.IsChecked) {
-				resultQuery = result.OrderBy(x => x.ItemArray[1]);
-			} else if(byType.IsChecked) {
-				resultQuery = result.OrderBy(x => x.ItemArray[2]);
-			} else {
-				resultQuery = result;
-			};
-
-
-			resultGrid.IsVisible = true;
-			ProductManage.IsVisible = true;
-			resultGrid.ItemsSource = resultQuery.CopyToDataTable().DefaultView;
+			resultGrid.ItemsSource = result;
 		}
 
+		private void Areas(object sender, EventArgs e) {
+			var result = from country in worldDS.Tables["country"].AsEnumerable()
+						 from max in (
+							from africa in worldDS.Tables["country"].AsEnumerable()
+							group africa by new {
+								id=africa.Field<string>("continent")
+							} into africaGrouped
+							where africaGrouped.Key.id.ToLower() == "africa"
+							select new { max = africaGrouped.Max(x => x.Field<float>("SurfaceArea")) }
 
-		private void FindProduct(object sender, EventArgs e) {
+							)
+						 where country.Field<float>("SurfaceArea") > max.max
+						 select new {
+							 Name = country.Field<string>("Name"),
+							 Area = country.Field<float>("SurfaceArea"),
+							 AfricaMaxArea = max.max
+						 };
 
-			var result = from m in shopDS.Tables["product"].AsEnumerable()
-						 where ((string)m["prod_name"]).Contains(productPattern.Text.ToString())
-						 select m;
+			resultGrid.ItemsSource = result;
+		}
+
+		private void Cities(object sender, EventArgs e) {
+			var population = from country in worldDS.Tables["country"].AsEnumerable()
+							 where country.Field<string>("Continent").ToLower() == "europe"
+							 select (int)country[6];
+
+			var result = from city in worldDS.Tables["city"].AsEnumerable()
+						 where (
+							from countryPop in population
+							where countryPop <= city.Field<int>("Population")
+							select countryPop).Count() == 3
+						 select city;
 
 			resultGrid.ItemsSource = result.CopyToDataTable().DefaultView;
-			ProductManage.IsVisible = true;
 		}
 
-
-
-		private void Refresh(object sender, EventArgs e) {
-
-			resultGrid.EndEdit();
-			//shopDS.Tables["product"].AcceptChanges();
-			//productAdapter.Up;
-
-			try {
-				productAdapter.Update(shopDS.Tables["product"]);
-			} catch(MySqlException ex) {
-				Debug.WriteLine(ex.Message);
-			}
-			shopDS.Clear();
-			manufAdapter.Fill(shopDS, "manufact");
-			productAdapter.Fill(shopDS, "product");
-			customerAdapter.Fill(shopDS, "customer");
-
-			PageLoaded(null, null);
-		}
-
-		private void AddProduct(object sender, EventArgs e) {
-			DataRow row;
-			row = shopDS.Tables["product"].NewRow();
-			row[0] = (int)(shopDS.Tables["product"].AsEnumerable()).Last()[0] + 1;
-			shopDS.Tables["product"].Rows.Add(row);
-
-			resultGrid.IsVisible = true;
-			resultGrid.ItemsSource = shopDS.Tables["product"].DefaultView;
-		}
-
-		private void RemoveProduct(object sender, EventArgs e) {
-			if(resultGrid.SelectedItem != null) {
-				//shopDS.Tables["product"].Rows.Remove((DataRow)((DataRowView)resultGrid.SelectedItem).Row);
-				//shopDS.Tables["product"].Rows.RemoveAt((int)resultGrid.SelectedIndex-1);
-				((DataRow)((DataRowView)resultGrid.SelectedItem).Row).Delete();
-				Refresh(null, null);
-			}
-		}
-
-
-		private void Group(object sender, EventArgs e) {
-			//var result = ((shopDS.Tables["product"].Select().Join(
-			//	shopDS.Tables["manufact"].Select(),
-			//	prod => prod[3],
-			//	manuf => manuf[0],
-			//	(prod, manuf) => new {
-			//		id = manuf[0],
-			//		Name = manuf[1],
-			//		Country = manuf[2],
-			//		Address = manuf[4],
-			//		Price = prod[4],
-			//		TotalQuantity = (int)prod[5]
-			//	})).GroupBy(x => x.id, (key, rest) => new {
-			//		id = key,
-			//		AVGprice = rest.Average(y => (float)y.Price),
-			//		TotalQuantity = rest.Sum(y => y.TotalQuantity)
-			//	})).Select(prop => new { prop.id, prop.AVGprice, prop.TotalQuantity });
-
-			var result2 = (shopDS.Tables["manufact"].Select()).GroupJoin(
-				(shopDS.Tables["product"].Select().Join(
-				shopDS.Tables["manufact"].Select(),
-				prod => prod[3],
-				manuf => manuf[0],
-				(prod, manuf) => new {
-					id = manuf[0],
-					Name = manuf[1],
-					Country = manuf[2],
-					Address = manuf[4],
-					Price = prod[4],
-					TotalQuantity = (int)prod[5]
-				})),
-					manuf => manuf[0],
-					key => key.id,
-					(manuf, stats) => new {
-						id = manuf[0],
-						Name = manuf[1],
-						Country = manuf[2],
-						Address = manuf[4],
-						AVGprice = stats.Average(prop => (float)prop.Price),
-						TotalQuantity = stats.Sum(prop => prop.TotalQuantity)
-					});
-
-			//var result1 = (shopDS.Tables["manufact"].Select()).Join(
-			//	result,
-			//	manuf => manuf[3],
-			//	stats => stats.id,
-			//	(manuf, stats) => new {
-			//		stats.id,
-			//		Name = manuf[1],
-			//		Country = manuf[2],
-			//		Address = manuf[4],
-			//		stats.AVGprice,
-			//		stats.TotalQuantity
-			//	})
-			//;
-
-			ProductManage.IsVisible = false;
-			resultGrid.ItemsSource = result2;
-
-		}
-
+		/*
 		private void GroupLINQ(object sender, EventArgs e) {
 			var result = from prod in shopDS.Tables["product"].AsEnumerable()
 						 join manuf in shopDS.Tables["manufact"].AsEnumerable()
