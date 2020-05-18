1d0a823d10f0e59ba9479bf68f3888971308da99
Danila <danila.litskevich@gmail.com>
2020-05-18 23:11:48 +0300

    init

---
+++ dbLabs/Classes/maskShop/Customers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dbLabsDummy.maskShop
+{
+    public partial class Customers
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CustomerType { get; set; }
+        public int? Discount { get; set; }
+    }
+}
