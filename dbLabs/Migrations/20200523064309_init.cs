f9cd49606d93172302d8ba5ad85d30cb7dfa6270
Danila <danila.litskevich@gmail.com>
2020-05-28 17:55:36 +0300

    save

--- dbLabs/Migrations/20200523064309_init.cs
+++ dbLabs/Migrations/20200523064309_init.cs
@@ -8,7 +8,15 @@ namespace dbLabsDummy.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            
+            var sp = @"CREATE PROCEDURE maskshop.viewproducts
+                    @amount_min int
+                AS
+                BEGIN
+                    SET NOCOUNT ON;
+					SELECT * FROM maskShop.ShopItems where amount>@amount_min
+                END";
+
+            migrationBuilder.Sql(sp);
 
         }
 
---
+++ dbLabsDummy/Migrations/20200528145156_sp-viewproducts.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dbLabsDummy.Migrations
+{
+    public partial class spviewproducts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+			var sp = @"CREATE PROCEDURE maskshop.[viewproducts]
+                    @amount_min int
+                AS
+                BEGIN
+                    SET NOCOUNT ON;
+					SELECT * FROM maskShop.ShopItems where amount>@amount_min
+                END";
+
+			migrationBuilder.Sql(sp);
+		}
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
