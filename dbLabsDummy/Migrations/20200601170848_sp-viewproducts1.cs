00679ffcc598424ee935f680c5efae3730bacbf1
Danila <danila.litskevich@gmail.com>
2020-06-01 21:13:19 +0300

    doneee

---
+++ dbLabsDummy/Migrations/20200601170848_sp-viewproducts1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dbLabsDummy.Migrations
+{
+    public partial class spviewproducts1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+              var sp = @"
+	CREATE PROCEDURE `viewproducts`()
+	BEGIN
+	SELECT * FROM maskShop.ShopItems where amount>100;
+	END;";
+
+		migrationBuilder.Sql (sp);
+                }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+		
+        }
+    }
+}
